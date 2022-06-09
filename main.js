9d7621ae83e635071d4df518cca98b063a7c0f7b
meteorstrike77 <89230784+meteorstrike77@users.noreply.github.com>
2022-06-08 20:40:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 music1 = "";
 music2 = "";
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 songstatus1 = "";
+songstatus2 = "";
 
 function preload() {
     music1 = loadSound("music.mp3");
@@ -26,7 +28,7 @@ function modelLoaded() {
 }
 function draw() {
     image(video, 0, 0, 600, 500);
-    songstatus1 = music1.isplaying();
+    songstatus1 = music1.isPlaying();
     fill("#FF0000");
     stroke("FF0000");
 
@@ -39,13 +41,29 @@ function draw() {
             document.getElementById("songname").innerHTML = "Song 1";
         }
     }
+    songstatus2 = music2.isPlaying();
+
+    if(scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        music1.stop();
+
+        if(songstatus2 == false) {
+            music2.play();
+            document.getElementById("songname").innerHTML = "Song 2"
+        }
+    }
     
 }
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("Left Wrist's Score = " + scoreLeftWrist);
+
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Right Wrist's Score = " + scoreRightWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         rightWristX = results[0].pose.rightWrist.x;
